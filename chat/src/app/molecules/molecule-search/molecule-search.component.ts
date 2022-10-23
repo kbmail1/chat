08c949463412add84110bafa8ad2382cbbf79c58
kundan Bapat <kundan.bapat@gmail.com>
2022-10-23 08:52:24 -0400

    basic working

--- chat/src/app/molecules/molecule-search/molecule-search.component.ts
+++ chat/src/app/molecules/molecule-search/molecule-search.component.ts
@@ -1,3 +1,4 @@
+import { MoleculeSearchService } from './molecule-search.service';
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Input } from '@angular/core';
 import { BehaviorSubject, debounceTime, distinctUntilChanged, filter, fromEvent, map, pipe, Subscription, take, takeWhile, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
@@ -10,31 +11,33 @@ import { HttpClient } from '@angular/common/http';
 export class MoleculeSearchComponent implements OnInit {
 
   @ViewChild('searchInput') searchInput!: ElementRef;
-
-  searchResults: any = []
-  searchText: string = ''
+  searchResults: string[] = [];
 
   constructor(
     protected httpClient: HttpClient,
+    public moleculeSearchService: MoleculeSearchService,
   ) { }
 
   ngOnInit(): void {
+    this.moleculeSearchService.searchText$.subscribe((searchResults: string[]) => {
+      this.searchResults = searchResults
+    })
   }
 
   ngAfterViewInit(): void {
-
     // https://fireflysemantics.medium.com/debouncing-your-angular-search-field-ce6686cf54b3
     fromEvent(this.searchInput.nativeElement, 'keyup')
       .pipe(
         filter(Boolean),
-        debounceTime(1500),
+        debounceTime(500),
         distinctUntilChanged(),
-        tap((text) => {
-          console.log(this.searchInput.nativeElement.value)
+        tap((text: any) => {
+          // console.log('hello')
+          // console.log(this.searchInput.nativeElement.value)
+          // console.log(`value: this.searchInput.nativeElement.value`)
+          this.moleculeSearchService.getSearchResults(this.searchInput.nativeElement.value)
         })
       )
-      .subscribe();
-
-
+      .subscribe()
   }
 }
--- chat/src/app/molecules/molecule-search/molecule-search.service.ts
+++ chat/src/app/molecules/molecule-search/molecule-search.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ElementRef } from '@angular/core';
+import { BehaviorSubject, Observable, debounceTime, distinctUntilChanged, filter, fromEvent, map, pipe, Subscription, take, takeWhile, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoleculeSearchService {
 
+  private searchTextSub = new BehaviorSubject<string[]>([]);
+  readonly searchText$ = this.searchTextSub.asObservable();
+
   constructor() { }
+
+  getSearchResults = (searchText: string) => {
+    const result = {
+      body: [
+        'one',
+        'two',
+        'three',
+      ]
+    }
+   return this.searchTextSub.next(result.body);
+  }
 }
